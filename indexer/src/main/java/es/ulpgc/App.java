c8520a6d9884bb869c89269b7a85d76ab34139ad
Guedess <albertoguedes4@gmail.com>
2025-11-09 18:57:27 +0000

    IndexerService removed for future remake

--- indexer/src/main/java/es/ulpgc/App.java
+++
@@ -1,7 +0,0 @@
-package es.ulpgc;
-
-
-public class App {
-    public static void main(String[] args) {
-    }
-}
--- indexer/src/main/resources/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS books (
-    book_id INTEGER PRIMARY KEY,
-                title TEXT,
-                author TEXT,
-                release_date TEXT,
-                language TEXT
-);
